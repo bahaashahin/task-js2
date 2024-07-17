934e9a78f4e062b64417657cc2145ac77ce726cd
bahaashahin <153877864+bahaashahin@users.noreply.github.com>
2024-07-17 18:50:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var entries = [];
+
+function data() {
+  var number = document.getElementById("number").value;
+  var name = document.getElementById("name").value;
+
+  // Add the new entry to the array
+  entries.push({ name: name, number: number });
+
+  // Create the table to display
+  var table =
+    "<table class='table table-bordered'><thead class='thead-light'><tr><th> N </th><th>Name</th><th>Number</th></tr></thead><tbody>";
+  for (var i = 0; i < entries.length; i++) {
+    table +=
+      "<tr><td>" +
+      (i + 1) +
+      "</td><td>" +
+      entries[i].name +
+      "</td><td>" +
+      entries[i].number +
+      "</td></tr>";
+  }
+  table += "</tbody></table>";
+
+  document.getElementById("contant").innerHTML = table;
+}
